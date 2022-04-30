c00c77630dee746c78e84af4f7d63046b1db2a7e
anielmelo <anielcontato@gmail.com>
2022-04-30 16:56:57 -0300

    Criação da página do cronômetro.
    Criei os elementos HTML, CSS e JS da página.

---
+++ stopwatch/app.js
@@ -0,0 +1,20 @@
+var hundredth = 0
+var second = 0
+var minute = 0
+var hour = 0
+
+function runner () {
+
+}
+
+function stopper () {
+
+}
+
+function reset () {
+
+}
+
+function stopwatch () {
+    
+}
