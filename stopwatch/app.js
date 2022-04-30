a7d99b407418cc8e51b18696d449400b13aaddc3
anielmelo <anielcontato@gmail.com>
2022-04-30 18:37:32 -0300

    Projeto Cronômetro finalizado.
    Terminei o cronômetro.

--- stopwatch/app.js
+++ stopwatch/app.js
@@ -1,20 +1,42 @@
-var hundredth = 0
 var second = 0
 var minute = 0
 var hour = 0
+var intervalWatch = 0
+
+function format (number) {
+    if (number < 10) {
+        return '0'+number
+    } else {
+        return number
+    }
+}
 
 function runner () {
-
+    stopwatch
+    intervalWatch = setInterval(stopwatch, 1000)
 }
 
 function stopper () {
-
+    clearInterval(intervalWatch)
 }
 
 function reset () {
-
+    hour = 0
+    minute = 0
+    second = 0
+    clearInterval(intervalWatch)
+    document.getElementById('textWatch').innerText = '00:00:00'
 }
 
 function stopwatch () {
-    
+    second ++
+    if (second == 60) {
+        minute ++
+        second = 0
+    }
+    if (minute == 60) {
+        hour ++
+        minute = 0
+    }
+    document.getElementById('textWatch').innerText = `${format(hour)}:${format(minute)}:${format(second)}`
 }
